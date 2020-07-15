dcfec4e906b8a5533cfb0e8d4b2bee45e4ee3b4e
PJ Gray <pj@evertrue.com>
2020-07-15 14:36:11 -0400

    Initial add of code

---
+++ PokerNowKit/PokerNowKit.h
@@ -0,0 +1,19 @@
+//
+//  PokerNowKit.h
+//  PokerNowKit
+//
+//  Created by PJ Gray on 7/15/20.
+//  Copyright © 2020 Say Goodnight Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PokerNowKit.
+FOUNDATION_EXPORT double PokerNowKitVersionNumber;
+
+//! Project version string for PokerNowKit.
+FOUNDATION_EXPORT const unsigned char PokerNowKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokerNowKit/PublicHeader.h>
+
+
